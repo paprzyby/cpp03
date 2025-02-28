cb542f3ff669578db18665f9acbdbd66615ea93d
Pawel Przybyla <pawel.przybyla2005@gmail.com>
2025-02-28 14:18:19 +0100

    initialized the four required member functions

--- ex00/ClapTrap.cpp
+++ ex00/ClapTrap.cpp
@@ -6,10 +6,46 @@
 /*   By: paprzyby <paprzyby@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/28 14:02:41 by paprzyby          #+#    #+#             */
-/*   Updated: 2025/02/28 14:02:46 by paprzyby         ###   ########.fr       */
+/*   Updated: 2025/02/28 14:18:00 by paprzyby         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
+ClapTrap::ClapTrap()
+{
+	std::cout << "Default constructor called." << std::endl;
+	hp = 10;
+	energy = 10;
+	ad = 0;
+}
 
+ClapTrap::ClapTrap(ClapTrap &copy)
+{
+	std::cout << "Copy constructor called." << std::endl;
+	*this = copy;
+}
+
+ClapTrap::ClapTrap(std::string name)
+{
+	std::cout << "Name constructor called" << std::endl;
+	this->name = name;
+}
+
+ClapTrap& ClapTrap::operator=(ClapTrap &copy)
+{
+	std::cout << "Copy assignment operator called." << std::endl;
+	if (this != &copy)
+	{
+		name = copy.name;
+		hp = copy.hp;
+		energy = copy.energy;
+		ad = copy.ad;
+	}
+	return (*this);
+}
+
+ClapTrap::~ClapTrap()
+{
+	std::cout << "Destructor called" << std::endl;
+}
--- ex00/ClapTrap.hpp
+++ ex00/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: paprzyby <paprzyby@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/28 14:01:45 by paprzyby          #+#    #+#             */
-/*   Updated: 2025/02/28 14:02:02 by paprzyby         ###   ########.fr       */
+/*   Updated: 2025/02/28 14:10:43 by paprzyby         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,22 @@
 
 class	ClapTrap
 {
+	public:
+		ClapTrap();
+		ClapTrap(ClapTrap &copy);
+		ClapTrap(std::string name);
+		ClapTrap &operator=(ClapTrap &copy);
+		~ClapTrap();
 
+		void attack(const std::string& target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+
+	private:
+		std::string	name;
+		int			hp;
+		int			energy;
+		int			ad;
 };
 
 #endif
